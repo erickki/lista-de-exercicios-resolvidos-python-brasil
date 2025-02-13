d4854566d9ece840a5dcba41d7aef9c56763cadc
Erick Rodrigues <erickrangelrodrigues@gmail.com>
2025-02-13 15:20:46 -0300

    Update

---
+++ 1.EstruturaSequencial/3.py
@@ -0,0 +1,5 @@
+#3.Faça um Programa que peça dois números e imprima a soma.
+numero1 = input("Escreva um número: ")
+numero2 = input("Escreva outro número: ")
+soma = float(numero1) + float(numero2)
+print(f"A soma dos números informados é {soma:.2f}")
